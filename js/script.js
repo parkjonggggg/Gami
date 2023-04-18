5396834bf1efb22c38cc4c4b04dfc6cbb9c861fa
parkjonggggg <131111503+parkjonggggg@users.noreply.github.com>
2023-04-18 11:56:13 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+	$(".notice> ul> li").click(function () {
+		$(this).children(".text_info").slideToggle();
+	});
+	$(".notice> ul> li").click(function () {
+		$(this).toggleClass("turn");
+
+		// if ($(this).hasClass("turn") === true) {
+		// 	$(this).siblings().removeClass("turn");
+
+		// }
+
+	});
+});
+
